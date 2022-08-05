b68aa304dd5567696a4d1c3d2afbb212df2f4be1
666WannaCry666 <usov3124@gmail.com>
2022-08-05 13:37:54 +0700

    Task 3 Done

--- Task_3/js/app.js
+++ Task_3/js/app.js
@@ -18,6 +18,32 @@ window.onload = () => {
     websocket = new WebSocket(wsUri);
     websocket.onopen = function(e) {
         writeMessage(`<span style="color: green;">Соединение установлено</span>`, "server");
+
+        btnSendNode.addEventListener("click", function() {
+            const message = inputNode.value;
+            writeMessage(message, "client");
+            websocket.send(message);
+            inputNode.value = "";
+        });
+        btnGeoNode.addEventListener("click", function() {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const { coords } = position;
+                    writeMessage(`<a href="https://www.openstreetmap.org/#map=15/${coords.latitude}/${coords.longitude}" target="_blank">Геолокация</a>`, "client");
+                });
+            }
+            else {
+                writeMessage(`<span style="color: red;">Ваше местоположение недоступно</span>`, "server");
+            }
+        });
+        inputNode.addEventListener("keydown", function(e) {
+            if (e.keyCode == 13) {
+                btnSendNode.click();
+            }
+        });
+
+        btnSendNode.style.backgroundColor = "#bad7ec";
+        btnGeoNode.style.backgroundColor = "#bad7ec";
     };
     websocket.onclose = function(e) {
         writeMessage(`<span style="color: red;">Соединение прервано</span>`, "server");
@@ -30,9 +56,3 @@ window.onload = () => {
     };
 };
 
-btnSendNode.onclick = () => {
-    const message = inputNode.value;
-    writeMessage(message, "client");
-    websocket.send(message);
-    inputNode.value = "";
-};
